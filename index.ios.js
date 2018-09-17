ac19df755d700e010f57a08b0a65ade65c3e22b8
wuyang <yang_wu@loongjoy.com>
2018-09-17 21:50:52 +0800

    feat:函数，状态

--- index.ios.js
+++ index.ios.js
@@ -5,25 +5,40 @@
  */
 
 import React, { Component } from 'react';
-import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
+import { AppRegistry, StyleSheet, Text, View, TextInput } from 'react-native';
+
+class Demo extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { txt: '' };
+  }
+
+  valueChange = m => {
+    this.setState({ txt: m });
+  };
+
+  render() {
+    return (
+      <View style={styles.democontainer}>
+        <Text style={styles.demo}>name: {this.props.name}</Text>
+        <Text>text: {this.state.txt}</Text>
+        <TextInput style={{height: 40}} placeholder="dadad!" onChangeText={(text)=>{this.valueChange(text)}} />
+      </View>
+    );
+  }
+}
 
 export default class ReactNativeApp extends Component {
   render() {
+    let txt = 'my fisrt app';
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
+        <Text style={styles.welcome}>{txt}</Text>
+        <Demo name="aaaaa" />
+        <Text style={styles.instructions}>To get started, edit index.ios.js</Text>
         <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
+          Press Cmd+R to reload,
+          {'\n'}
           Cmd+D or shake for dev menu
         </Text>
       </View>
@@ -32,22 +47,30 @@ export default class ReactNativeApp extends Component {
 }
 
 const styles = StyleSheet.create({
+  democontainer: {
+    width: 100,
+    height: 100,
+    backgroundColor: 'red'
+  },
+  demo: {
+    fontSize: 24
+  },
   container: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    backgroundColor: '#F5FCFF'
   },
   welcome: {
     fontSize: 20,
     textAlign: 'center',
-    margin: 10,
+    margin: 10
   },
   instructions: {
     textAlign: 'center',
     color: '#333333',
-    marginBottom: 5,
-  },
+    marginBottom: 5
+  }
 });
 
 AppRegistry.registerComponent('ReactNativeApp', () => ReactNativeApp);
